77d7b6639395dee36b08e0b4313ec4f1bab8b02a
navneetjha190 <63304335+navneetjha190@users.noreply.github.com>
2021-03-25 17:48:00 +0530

    React app

---
+++ src/index.js
@@ -0,0 +1,128 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+
+ReactDOM.render(
+  <html>
+  <head>
+    <title>DSI web landing page</title>
+    <meta name="viewport" content="width-device-width, initial-scale=1"></meta>
+  </head>
+  <body>
+    <div class="n1">
+      <div class="n11">
+      </div>
+      <div class="n12">
+      </div>
+    <div class="n13">
+    </div>
+    <div class="n14">
+    </div>
+    <div class="n21">
+    </div>
+    <div class="n22">
+    </div>
+    <div class="n23">
+    </div>
+    <div class="n31">
+    </div>
+    <div class="n32">
+    </div>
+    <div class="n33">
+    </div>
+    <div class="n41">
+    </div>
+    <div class="n42">
+    </div>
+    <div class="n43">
+    </div>
+    <div class="n44">
+    </div>
+    <div class="n51">
+    </div>
+    <div class="n52">
+    </div>
+    <div class="n53">
+    </div>
+    <div class="n54">
+    </div>
+    <div class="n61">
+    </div>
+    <div class="n62">
+    </div>
+    <div class="n63">
+    </div>
+    <div class="n71">
+    </div>
+    <div class="n72">
+    </div>
+    <div class="n73">
+    </div>
+    <div class="n74">
+    </div>
+</div>
+<div class="k2">
+  <div class="k21">
+    <div class="k211">D</div>
+    <div class="k212">S</div>
+    <div class="k213">I</div>
+    <div class="k214">DWIDASA</div>
+    <div class="k215">SAMSARA</div>
+    <div class="k216">INDONESIA</div>
+    <div class="k217">Services</div>
+    <div class="k218">Product</div>
+    <div class="k219">Technology</div>
+    <div class="k210">About</div>
+    <img class="k2101" src="arrow.jpg"></img>
+    <div class="k2102">Client</div>
+    <img class="k2103" src="arrow.jpg"></img>
+    <div class="k2104">Partner</div>
+    <img class="k2105" src="union.jpg"></img>
+    <img class="k2106" src="mail.jpg"></img>
+    <img class="k2107" src="shuffle.jpg"></img>
+  </div>
+  <div class="k22">Making the most of the ever-growing</div> 
+  <div class="k221" >Information Technology</div>
+  <div class="k222">Managed by a team of professional experts with extensive experience and impressive track records</div>
+  <div class="k223">
+    <div class="k2231">Read more</div>
+  </div>
+  <img class="k224" src="devices/mobile.jpg"></img>
+  <img class="k2241" src="devices/computer.jpg"></img>
+  <img class="k2242" src="devices/plant.jpg"></img>
+  <img class="k2243" src="devices/tablet.jpg"></img>
+  <img class="k2244" src="devices/character.jpg"></img>
+  
+   
+    
+    <img class="k2312" src="browser-stats/device.jpg"></img>
+    <img class="k2313" src="browser-stats/coffee.jpg"></img>
+    <img class="k2314" src="browser-stats/desk.jpg"></img>
+    <img class="k2311" src="browser-stats/character.jpg"></img>
+   <div class="k2315">Welcome to</div>
+   <div class="k2316"> Dwidasa Samsara</div>
+   <div class="k2317">Indonesia (DSI)</div>
+   <div class="k2318">Dwidasa Samsara Indonesia (DSI) was firstly established in 2010 by the founders, who each of them has a common end objective to innovate new creations by making the most of the ever-growing information technology through DSI’s distinct front-end based application concept.
+
+Managed by a team of professional experts with extensive experience and impressive track records, DSI believes that continuous improvements and innovations assure your business to run effectively and efficiently.</div>
+
+   <div class="k31">Product and Service</div>
+    <img class="k311" src="devices/Frame_2.jpg"></img>
+    <img class="k312" src="devices/Frame_1.jpg"></img>
+    <img class="k313" src="devices/Frame_3.jpg"></img>
+    <img class="k314" src="devices/Frame_5.jpg"></img>
+
+
+</div>
+
+  </body>
+  </html>,
+  document.getElementById('root')
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
